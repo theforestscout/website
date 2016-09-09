fb8cd840054ab56ef1013c8eeb860828cfd08552
Bryan Willis <willis.bryan@live.com>
2016-09-09 01:05:52 -0500

    Update page-contributors.php

--- page-contributors.php
+++ page-contributors.php
@@ -16,25 +16,19 @@ remove_action('genesis_loop', 'genesis_do_loop');
 add_action('genesis_loop', 'fs_do_author_loop');
 function fs_do_author_loop(){ ?>
 <h1 class="page-header text-center mb-4"><?php the_title(); ?></h1>
-
 <div class="authorsContainer">
-
 <?php
-
 // number of users to return
 $howManyAuthors = 500;
-
 // Get users and count of posts put into array
-
 $authorsArray = array();
+// Roles to display
 $blogusers = get_users(['role__in' => ['editor', 'author']]);
-
 if ($blogusers) {
 	foreach($blogusers as $bloguser) {
 		$post_count = count_user_posts($bloguser->ID);
 		$authorsArray[$bloguser->ID] = $post_count;
 	}
-
 	arsort($authorsArray);
 	$maxauthor = $howManyAuthors;
 	$count = 0;
@@ -53,75 +47,55 @@ if ($blogusers) {
 			$gplus = $user->googleplus;
 			$author_posts_url = get_author_posts_url($key);
 			$post_count = $value;
+?>
 <div class="media author-bio">
-    <div class="pull-left" style="width: 100px; height: 100px;">
-        <div class="border-right"><?php echo get_avatar( $id, 96, '', '', null ); ?></div>
-    </div>
-    <div class="bio-data media-body">
-            <p class="bio">
-                <a class="text-primary" href="<?php echo $author_posts_url; ?>"><strong class="displayname"><?php echo $displayname; ?></strong></a> <span class="bio-description"><?php echo $bio; ?></span>
-            </p>
-        <div class="flex_grid recent-contributions">
-        <div class="flex_col-9_xs-12"> 
-                    <?php
-                    //*
-                          $args=array(
-                            'showposts'=>3,
-                            'numberposts'=>3,
-                            'author'=>$user->ID,
-                            'caller_get_posts'=>3
-                          );
-                         // */
-
-
-
-                          $my_query = new WP_Query($args);
-                          if( $my_query->have_posts() ) {
-                            ?>
-                            <strong><em>Latest Contributions:</em></strong><br>
-                            <?php
-                            while ($my_query->have_posts()) : $my_query->the_post();
-                        ?>
-                        <span style="display: block; margin-top: 5px;"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permenent Link To <?php the_title_attribute(); ?>"><?php the_title(); ?><small class="latest-sm">&nbsp;(<?php the_time('m.d.y') ?>)</small></a></span>
-                      <?php
-                            endwhile;
-                         }
-                      ?>
-        </div>
-        <div class="buttons flex_col-3_xs-12"> 
-            <div class="social">
-            <?php 
-                if ( $email ) {
-                    echo '<a class="icon-email-black" href="mailto:'.$email.'"><i class="fa fa-envelope"></i></a>';
-                }
-
-                if ( $facebook ) {
-                    echo '<a class="text-btn icon-facebook-black" href="'.$facebook.'"><i class="fa fa-facebook"></i></a>';
-                }
-                if ( $gplus ) {
-                    echo '<a class="text-btn icon-google-plus-black" href="'.$gplus.'"><i class="fa fa-google-plus"></i></a>';
-                }
-                if ( $twitter ) {
-                    echo '<a href="https://twitter.com/'.$twitter.'" class="text-btn icon-twitter-black"><i class="fa fa-twitter"></i></a>';
-                }
-            ?>
-            </div>
-        </div>
-        </div>
-
-
-    </div>
+	<div class="pull-left" style="width: 100px; height: 100px;">
+		<div class="border-right">
+			<?php echo get_avatar($id, 96, '', '', null); ?>
+		</div>
+	</div>
+	<div class="bio-data media-body">
+		<p class="bio">
+			<a class="text-primary" href="<?php echo $author_posts_url; ?>"><strong class="displayname"><?php echo $displayname; ?></strong></a> <span class="bio-description"><?php echo $bio; ?></span>
+		</p>
+		<div class="flex_grid recent-contributions">
+			<div class="flex_col-9_xs-12"> 
+			<?php
+			$args = array(
+				'showposts' => 3,
+				'numberposts' => 3,
+				'author' => $user->ID,
+				'caller_get_posts' => 3
+			);
+			$my_query = new WP_Query($args);
+			if ($my_query->have_posts()): ?>
+				<strong><em>Latest Contributions:</em></strong><br />
+				<?php while ($my_query->have_posts()):
+				$my_query->the_post(); ?>
+				<span class="d-block mt-5"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permenent Link To <?php the_title_attribute(); ?>"><?php the_title(); ?><small class="latest-sm">&nbsp;(<?php the_time('m.d.y') ?>)</small></a></span>
+				<?php endwhile; ?>
+				<?php endif; ?>
+			</div><!-- // .flex_col-* -->
+			<div class="buttons flex_col-3_xs-12">
+				<div class="social fa-social-color">
+				<?php
+				if ($email) 
+					echo '<a class="icon-email-black" href="mailto:' . $email . '"><i class="fa fa-envelope"></i></a>';
+				if ($facebook) 
+					echo '<a class="text-btn icon-facebook-black" href="' . $facebook . '"><i class="fa fa-facebook"></i></a>';
+				if ($gplus) 
+					echo '<a class="text-btn icon-google-plus-black" href="' . $gplus . '"><i class="fa fa-google-plus"></i></a>';
+				if ($twitter) 
+					echo '<a href="https://twitter.com/' . $twitter . '" class="text-btn icon-twitter-black"><i class="fa fa-twitter"></i></a>';
+				?>
+				</div><!-- // .social -->
+			</div><!-- // .flex_col-* -->
+		</div><!-- // .flex_grid -->
+	</div><!-- // .bio-data -->
 </div>
-
-<?php 
-
-                }
-              }
-            }
-            ?>
-
-</div>
-   <?php 
-}
-
+<?php
+		} // endifif;
+	} // endforeach;
+} // endif;
+echo '</div><!-- // .authorsContainer-->';
 genesis();
